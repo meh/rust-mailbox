61fe40fe212a4f10f7b56cdbe9e12c53f6aa3231
meh <meh@schizofreni.co>
2016-08-28 21:37:45 +0200

    examples/count: update to new API

--- examples/count.rs
+++ examples/count.rs
@@ -4,16 +4,15 @@ use std::fs::File;
 use std::env;
 
 fn main() {
-	let mbox = mailbox::read(File::open(env::args().nth(1).expect("no file given")).unwrap());
-
+	let mut mbox  = mailbox::read(File::open(env::args().nth(1).expect("no file given")).unwrap());
 	let mut read  = 0;
 	let mut total = 0;
 
-	for mail in mbox {
+	for mail in mbox.body(false) {
 		if let Ok(mail) = mail {
 			total += 1;
 
-			if let Some(&mailbox::mail::Header::Status(status)) = mail.headers().get("Status") {
+			if let Some(Ok(mailbox::mail::Header::Status(status))) = mail.headers().get("Status") {
 				if status.contains(mailbox::mail::status::SEEN) {
 					read += 1;
 				}
