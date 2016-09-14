54124d3622680db2e56af80fd873cf8f0aa833b2
meh <meh@schizofreni.co>
2016-09-14 02:20:13 +0200

    example/status: support multiple mboxes

--- examples/status.rs
+++ examples/status.rs
@@ -18,42 +18,44 @@ pub struct Status {
 fn main() {
 	let mut status = Status::default();
 
-	for mail in mailbox::read(File::open(env::args().nth(1).expect("no file given")).unwrap()).body(false) {
-		if let Ok(mail) = mail {
-			let mut current = header::Status::empty();
+	for path in env::args().skip(1) {
+		for mail in mailbox::read(File::open(path).unwrap()).body(false) {
+			if let Ok(mail) = mail {
+				let mut current = header::Status::empty();
 
-			if let Some(Ok(s)) = mail.headers().get::<header::Status>() {
-				current |= s;
-			}
+				if let Some(Ok(s)) = mail.headers().get::<header::Status>() {
+					current |= s;
+				}
 
-			if let Some(Ok(s)) = mail.headers().get_from::<header::Status, _>("X-Status") {
-				current |= s;
-			}
+				if let Some(Ok(s)) = mail.headers().get_from::<header::Status, _>("X-Status") {
+					current |= s;
+				}
 
-			status.total += 1;
+				status.total += 1;
 
-			if current.contains(header::status::SEEN) {
-				status.seen += 1;
-			}
+				if current.contains(header::status::SEEN) {
+					status.seen += 1;
+				}
 
-			if current.contains(header::status::OLD) {
-				status.old += 1;
-			}
+				if current.contains(header::status::OLD) {
+					status.old += 1;
+				}
 
-			if current.contains(header::status::ANSWERED) {
-				status.answered += 1;
-			}
+				if current.contains(header::status::ANSWERED) {
+					status.answered += 1;
+				}
 
-			if current.contains(header::status::FLAGGED) {
-				status.flagged += 1;
-			}
+				if current.contains(header::status::FLAGGED) {
+					status.flagged += 1;
+				}
 
-			if current.contains(header::status::DRAFT) {
-				status.draft += 1;
-			}
+				if current.contains(header::status::DRAFT) {
+					status.draft += 1;
+				}
 
-			if current.contains(header::status::DELETED) {
-				status.deleted += 1;
+				if current.contains(header::status::DELETED) {
+					status.deleted += 1;
+				}
 			}
 		}
 	}
